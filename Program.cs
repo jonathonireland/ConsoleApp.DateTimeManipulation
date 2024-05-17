154bc8af49f399b272d73f0da8cf3451a331c52e
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-17 12:28:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Date and Time
+Console.WriteLine("************ - DateTime Manipulation - *************");
+
+// Empty DateTime object
+DateTime dateTime = new DateTime();
+
+// Create a DateTime from date and time
+var dateOfBirth = new DateTime(1980, 12, 25);
+Console.WriteLine($"My Dob is: {dateOfBirth}");
+
+var exactDateAndTimeOfBirth = new DateTime(1980, 12, 25, 14, 45, 25, DateTimeKind.Local);
+Console.WriteLine($"My Dob is: {exactDateAndTimeOfBirth}");
+
+Console.WriteLine($"Day of Week: {dateOfBirth.DayOfWeek}");
+Console.WriteLine($"Day of Year: {dateOfBirth.DayOfYear}");
+Console.WriteLine($"Time of Day: {exactDateAndTimeOfBirth.TimeOfDay}");
+Console.WriteLine($"Tick: {exactDateAndTimeOfBirth.Ticks}");
+Console.WriteLine($"Kind: {exactDateAndTimeOfBirth.Kind}");
+
+// Create Date Time from current timestamp
+DateTime now = DateTime.Now;
+Console.WriteLine("The Time Now is: "+ now);
+
+// Create a DateTime from a String
+Console.WriteLine("What is your DOB (dd/mm/yyyy): ");
+string dob = Console.ReadLine();
+
+var userDob = DateTime.Parse(dob);
+Console.WriteLine($"Day of Week: {userDob.DayOfWeek}");
+Console.WriteLine($"Day of Year: {userDob.DayOfYear}");
+Console.WriteLine($"Time of Day: {userDob.TimeOfDay}");
+Console.WriteLine($"Tick: {userDob.Ticks}");
+Console.WriteLine($"Kind: {userDob.Kind}");
+
+// Change the format of a date time:
+Console.WriteLine($"Formatted Date: {userDob.ToString("dd/MM/yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob.ToString("MMM dd, yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob.ToString("dd-MMM-yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob:dddd-MMMM-yyyy}"); // interpolation only
+
+// Add Additional Time
+Console.WriteLine($"One Hour from now is: {now.AddHours(1)}");
+Console.WriteLine($"One Day from now is: {now.AddDays(1)}");
+Console.WriteLine($"One day ago from now is {now.AddDays(-1)}");
+
